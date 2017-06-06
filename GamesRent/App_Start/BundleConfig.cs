778d71d08e2659a79c56dd18d3083a5a50c7256f
AnkitVartak1993 <ankitvartak@gmail.com>
2017-06-06 13:25:20 -0400

    Bootstrap added properly

--- GamesRent/App_Start/BundleConfig.cs
+++ GamesRent/App_Start/BundleConfig.cs
@@ -20,11 +20,11 @@ namespace GamesRent
                         "~/Scripts/modernizr-*"));
 
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/bootstrap-a.js",
+                      "~/Scripts/bootstrap.js",
                       "~/Scripts/respond.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
+                      "~/Content/bootstrap-a.css",
                       "~/Content/site.css"));
         }
     }
