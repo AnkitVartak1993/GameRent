fd3aea92944a75bef123e3ef43d15a85d3480cbd
AnkitVartak1993 <ankitvartak@gmail.com>
2017-06-06 13:23:23 -0400

    Added new Bootstrap

--- GamesRent/App_Start/BundleConfig.cs
+++ GamesRent/App_Start/BundleConfig.cs
@@ -20,7 +20,7 @@ namespace GamesRent
                         "~/Scripts/modernizr-*"));
 
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/bootstrap.js",
+                      "~/Scripts/bootstrap-a.js",
                       "~/Scripts/respond.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
