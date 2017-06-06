2120e086ebedc6fc01b38d093729cb9fd3f5e894
AnkitVartak1993 <ankitvartak@gmail.com>
2017-06-06 13:19:45 -0400

    Add project files.

---
+++ GamesRent/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(GamesRent.Startup))]
+namespace GamesRent
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
