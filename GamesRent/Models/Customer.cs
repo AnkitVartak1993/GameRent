5c7b1b53a3e2326d63f5ad098f8d1f7baa1bf61c
AnkitVartak1993 <ankitvartak@gmail.com>
2017-06-06 16:18:33 -0400

    Altered Customer Model and Dbupdate

--- GamesRent/Models/Customer.cs
+++ GamesRent/Models/Customer.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.ComponentModel.DataAnnotations;
 
 namespace GamesRent.Models
 {
@@ -9,6 +10,8 @@ namespace GamesRent.Models
     {
         public int Id { get; set; }
 
+        [Required]
+        [StringLength(255)]
         public String Name { get; set; }
 
         public bool IsSubscribedtoNews { get; set; }
