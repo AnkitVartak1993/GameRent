5b1c8f295edea074b3117aa328b0974ca0198d75
AnkitVartak1993 <ankitvartak@gmail.com>
2017-06-06 13:35:12 -0400

    Added Game mvc blank

---
+++ GamesRent/Controllers/GamesController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using GamesRent.Models;
+
+namespace GamesRent.Controllers
+{
+    public class GamesController : Controller
+    {
+        // GET: Games
+        public ActionResult Index()
+        {
+            var game = new Game() { Name = "GTA 5" };
+            return View(game);
+        }
+    }
+}
