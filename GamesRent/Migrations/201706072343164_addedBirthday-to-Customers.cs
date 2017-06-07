f644b76d3d649d8eb65cdac037d651bb865dc2a2
AnkitVartak1993 <ankitvartak@gmail.com>
2017-06-07 19:55:02 -0400

    Added Birthdate to customers and DBupdate

---
+++ GamesRent/Migrations/201706072343164_addedBirthday-to-Customers.cs
@@ -0,0 +1,18 @@
+namespace GamesRent.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class addedBirthdaytoCustomers : DbMigration
+    {
+        public override void Up()
+        {
+            AddColumn("dbo.Customers", "BirthDate", c => c.DateTime());
+        }
+        
+        public override void Down()
+        {
+            DropColumn("dbo.Customers", "BirthDate");
+        }
+    }
+}
