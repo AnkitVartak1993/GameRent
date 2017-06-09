8b2eda303ecf4d10a6b01ba151ef95c4e9a1da27
AnkitVartak1993 <ankitvartak@gmail.com>
2017-06-09 13:36:08 -0400

    Added custom validation min18years and added clientside validation

--- GamesRent/Models/Customer.cs
+++ GamesRent/Models/Customer.cs
@@ -20,6 +20,7 @@ namespace GamesRent.Models
         [Display(Name ="Membership Type")]
         public byte MembershipTypeId { get; set; }
 
+        [Min18year]
         public DateTime? BirthDate { get; set; }
 
     }
---
+++ GamesRent/Models/Min18year.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+namespace GamesRent.Models
+{
+    public class Min18year: ValidationAttribute
+    {
+
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+
+            var customer = (Customer)validationContext.ObjectInstance;
+
+            if (customer.MembershipTypeId == 1 || customer.MembershipTypeId == 0)
+                return ValidationResult.Success;
+            if (customer.BirthDate == null)
+                return new ValidationResult("Please Enter Birthdate");
+
+            var age = DateTime.Today.Year - customer.BirthDate.Value.Year;
+
+            return age>=18 ?  ValidationResult.Success : new ValidationResult("Only 18 years and above person is eligible for this membership");
+          
+        }
+
+    }
+}
