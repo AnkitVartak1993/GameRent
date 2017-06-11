1cd5862c2dcf6c732e47c400284b4517fbf840d2
AnkitVartak1993 <ankitvartak@gmail.com>
2017-06-11 17:33:02 -0400

    Added Rental ModelController and DTO for it and DBupdate

---
+++ GamesRent/Models/Rental.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ComponentModel.DataAnnotations;
+
+namespace GamesRent.Models
+{
+    public class Rental
+    {
+        public int Id { get; set; }
+
+        
+        public Customer Customer { get; set; }
+
+        public Game Game { get; set; }
+
+        public DateTime DateRented { get; set; }
+
+        public DateTime? DateReturned { get; set; }
+
+    }
+}
